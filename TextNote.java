4f97bece945e657d52be98c323b007fc67ebd451
bw15485 <47212088+bw15485@users.noreply.github.com>
2020-11-03 12:34:04 +0000

    Create TextNote.java

---
+++ TextNote.java
@@ -0,0 +1,14 @@
+public class TextNote 
+{
+    private String message;
+
+    public TextNote(String s)
+    {
+        message = s;
+    }
+
+    public String getMessage()
+    {
+        return message;
+    }
+}
